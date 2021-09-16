be171865ba0492db0698cf705c5e04d31ff5ed9e
Varinder jassal <varinderjassal@Varinders-MacBook-Pro.local>
2021-09-16 10:49:51 -0700

    adding files to new branch

--- src/com/company/exceptions/AgeNotCorrectException.java
+++
@@ -1,8 +0,0 @@
-package com.company.exceptions;
-
-public class AgeNotCorrectException extends Exception {
-    String s ="";
-    public AgeNotCorrectException(String s) {
-        this.s =s;
-    }
-}
--- src/com/company/exceptions/NameDeclarationWrong.java
+++
@@ -1,5 +0,0 @@
-package com.company.exceptions;
-
-public class NameDeclarationWrong extends Exception{
-
-}
